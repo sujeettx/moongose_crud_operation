99716dac6034125661b6b782505adee239f74a41
sujeet kushwaha <sujeetkushwaha2802gmail.com>
2024-08-01 10:38:11 +0530

    middileware added in server.js file

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const bodyparser = require("body-parser");
 const app = express();
 const port = 5800;
 require("./db");
+const logrequest = (req,res,next)=>{
+  console.log(`[${ new Date().toLocaleString()} request made to : ${req.originalUrl}]`);
+  next();
+}
+app.use(logrequest);
 app.use(express.json());
 const menucard = require('./routes/menucardRoutrs');
 app.use('/menuCard',menucard);
